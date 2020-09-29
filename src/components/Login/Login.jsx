0d0f1fe3288bd565a0c1c917c2d0a217940ce7e5
Oleksandr Levak <sashavytvyckyj@gmail.com>
2020-09-29 22:55:41 +0300

    Login Form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,11 +6,16 @@ class Login extends Component {
   constructor(props) {
     super(props);
   }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    alert(e.target.Password.value);
+  }
   render() {
     return (
       <Modal
         {...this.props}
-        size="lg"
+        size="md"
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
@@ -18,10 +23,47 @@ class Login extends Component {
           <Modal.Title id="contained-modal-title-vcenter">Login</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className="container">Form fill</div>
+          <div className="container">
+            <Row>
+              <Col sm={12}>
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Group controlId="Login">
+                    <Form.Label>Login</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="login"
+                      required
+                      placeholder="Login"
+                    ></Form.Control>
+                  </Form.Group>
+                  <Form.Group controlId="Password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      required
+                      placeholder="Password"
+                    ></Form.Control>
+                  </Form.Group>
+                  <Form.Group>
+                    <Button variant="dark" type="submit" block>
+                      Login
+                    </Button>
+                  </Form.Group>
+                  <Form.Group>
+                    <Button variant="dark" href="#" block>
+                      Register
+                    </Button>
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
+          </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={this.props.onHide}>Close</Button>
+          <Button variant="dark" onClick={this.props.onHide}>
+            Close
+          </Button>
         </Modal.Footer>
       </Modal>
     );
