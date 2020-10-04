46f8f3e56f117e6d4e6c4d066a2e8dff69b0563d
Oleksandr Levak <sashavytvyckyj@gmail.com>
2020-10-04 22:36:30 +0300

    employee card

--- src/components/Employees/Employee/Employee.jsx
+++ src/components/Employees/Employee/Employee.jsx
@@ -11,12 +11,13 @@ const Employee = (props) => {
             <img className={s.vacancyImg} src={myPhoto} />
           </div>
           <div className={s.vacancyInfoText}>
-            <div className={s.vacancyTitile}>{props.employee.FirstName}</div>
-            <div className={s.company}>{props.employee.Surname}</div>
-            <div className={s.vacancyDescription}>
-              {props.employee.Education}
+            <div className={s.fio}>
+              {props.employee.FirstName} {props.employee.Surname}
             </div>
-            <div className={s.salary}>{props.employee.Birthday}</div>
+            <div className={s.gender}>{props.employee.Gender}</div>
+            <div className={s.education}>{props.employee.Education}</div>
+            <div className={s.contact}>{props.employee.Mail}</div>
+            <div className={s.contact}>{props.employee.ContactNumber}</div>
           </div>
         </div>
       </div>
