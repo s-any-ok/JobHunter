d67712c45dc62e65dd8f5206f8ae1386ec580da4
Oleksandr Levak <sashavytvyckyj@gmail.com>
2020-10-04 18:38:54 +0300

    NavBar

--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import s from "./HeaderNav.module.css";
-import { NavLink } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import Login from "../Login/Login";
+import NavBar from "./Nav/NavBar";
 
 const HeaderNav = () => {
   const [modalShow, setModalShow] = useState(false);
@@ -13,23 +13,7 @@ const HeaderNav = () => {
           <big>JH</big>
         </div>
         <div>
-          <div className={s.navbar}>
-            <NavLink to="/vacancies" activeClassName={s.active}>
-              Vacancies
-            </NavLink>
-            <NavLink to="/companies" activeClassName={s.active}>
-              Companies
-            </NavLink>
-            <NavLink to="/employees" activeClassName={s.active}>
-              Employees
-            </NavLink>
-            <NavLink to="/news" activeClassName={s.active}>
-              News
-            </NavLink>
-            <NavLink to="/about" activeClassName={s.active}>
-              About
-            </NavLink>
-          </div>
+          <NavBar />
         </div>
         <div className={s.login}>
           <Button
---
+++ src/components/HeaderNav/Nav/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import s from "./NavBar.module.css";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className={s.navbar}>
+      <NavLink to="/vacancies" activeClassName={s.active}>
+        Vacancies
+      </NavLink>
+      <NavLink to="/companies" activeClassName={s.active}>
+        Companies
+      </NavLink>
+      <NavLink to="/employees" activeClassName={s.active}>
+        Employees
+      </NavLink>
+      <NavLink to="/news" activeClassName={s.active}>
+        News
+      </NavLink>
+      <NavLink to="/about" activeClassName={s.active}>
+        About
+      </NavLink>
+    </div>
+  );
+};
+
+export default NavBar;
