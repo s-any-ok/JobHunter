a07791cfde61f07e6612c194094620aabd60c575
Oleksandr Levak <sashavytvyckyj@gmail.com>
2020-10-20 20:58:05 +0300

    Updates

---
+++ precache-manifest.1bb5e43e2ecb9c747938a83197da16f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b80d32debaeaf8d72e909318ec54d0",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "db55c8aff8214c9d4448",
+    "url": "/SocialNetwork/static/css/main.bae1dab2.chunk.css"
+  },
+  {
+    "revision": "e9525e92c10d86f51f0f",
+    "url": "/SocialNetwork/static/js/2.a275610c.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/SocialNetwork/static/js/2.a275610c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db55c8aff8214c9d4448",
+    "url": "/SocialNetwork/static/js/main.537f8b32.chunk.js"
+  },
+  {
+    "revision": "354d8bdc2be4c5880230",
+    "url": "/SocialNetwork/static/js/runtime-main.d70f2951.js"
+  },
+  {
+    "revision": "bc7007d0b2e31d33ab828ae59b00659a",
+    "url": "/SocialNetwork/static/media/I.bc7007d0.jpg"
+  },
+  {
+    "revision": "95f81f823adb4fe596de1cdfb463b9e5",
+    "url": "/SocialNetwork/static/media/authUser.95f81f82.png"
+  },
+  {
+    "revision": "78d4daa298e8cc1077ef3580424cc8aa",
+    "url": "/SocialNetwork/static/media/jh.78d4daa2.png"
+  }
+]);
